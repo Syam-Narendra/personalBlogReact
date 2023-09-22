d48e736489b77640e4f40c11ca7e59a5ea812ea9
Syam Narendra <syamnarendra85@gmail.com>
2023-09-22 20:37:07 +0530

    description changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,7 +14,7 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h1>{`Hi! I'm Syam Narendra`} <br/><span className="tagline wrap">Full Stack Developer</span></h1>
-                <p>Highly skilled and motivated full-stack developer, Experience in creating web applications from concept to deployment. I have a passion for both front-end and back-end development, and I thrive in dynamic, collaborative environments. My dedication to staying current with the latest technologies allows me to deliver efficient and innovative solutions to complex problems.</p>
+                <p>Highly skilled and motivated full-stack developer, Experience in creating web applications from concept to deployment. I have a passion for both front-end and back-end development, and I thrive in dynamic, collaborative environments.</p>
                   <a className="lets-connect-anchor" href="#connect">Let’s Connect <ArrowRightCircle size={25}/></a>
               </div>}
             </TrackVisibility>
